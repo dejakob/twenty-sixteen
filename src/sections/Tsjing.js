e0cc8bf58cd7bc8d8e39e90f5098fdbfa26d7acf
Jakob Kerkhove <jakobvti@gmail.com>
2016-12-27 20:51:00 +0100

    Add some April timeline items

--- src/sections/Tsjing.js
+++ src/sections/Tsjing.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Section, VerticalLine, TimelineBlock, CalendarDay, FacebookPost } from '../ui';
+import { Section, VerticalLine, TimelineBlock, CalendarDay, FacebookPost, H1 } from '../ui';
 
 class Tsjing extends Component
 {
@@ -22,6 +22,36 @@ class Tsjing extends Component
                         href="https://www.facebook.com/dejakob/posts/1068226946549391"
                     />
                     <H1><i className="fa fa-beer"></i> I suddenly started doing professional research...</H1>
+                </TimelineBlock>
+
+                <VerticalLine />
+
+                <TimelineBlock>
+                    <CalendarDay
+                        month="April"
+                        day={3}
+                    />
+                </TimelineBlock>
+
+                <TimelineBlock>
+                    <FacebookPost
+                        href="https://www.facebook.com/dejakob/posts/1069235383115214"
+                    />
+                </TimelineBlock>
+
+                <VerticalLine />
+
+                <TimelineBlock>
+                    <CalendarDay
+                        month="April"
+                        day={8}
+                    />
+                </TimelineBlock>
+
+                <TimelineBlock>
+                    <FacebookPost
+                        href="https://www.facebook.com/dejakob/posts/1072661902772562"
+                    />
                 </TimelineBlock>
 
                 <VerticalLine />

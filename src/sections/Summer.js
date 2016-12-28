503b1948b2aa5898dac0ffef865afd8632060d32
Jakob Kerkhove <jakobvti@gmail.com>
2016-12-28 20:25:50 +0100

    Add another item to June

--- src/sections/Summer.js
+++ src/sections/Summer.js
@@ -84,6 +84,21 @@ class Summer extends Component
                 <TimelineBlock>
                     <CalendarDay
                         month="June"
+                        day={26}
+                    />
+                </TimelineBlock>
+
+                <TimelineBlock>
+                    <FacebookPost
+                        href="https://www.facebook.com/photo.php?fbid=1120922107946541&set=a.452807001424725.98241.100000862866272&type=3&theater"
+                    />
+                </TimelineBlock>
+
+                <VerticalLine />
+
+                <TimelineBlock>
+                    <CalendarDay
+                        month="June"
                         day={19}
                     />
                 </TimelineBlock>

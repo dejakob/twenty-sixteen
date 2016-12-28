09c302e238ab0fc308817eab8a500ae3b69e2ba1
Jakob Kerkhove <jakobvti@gmail.com>
2016-12-28 20:15:10 +0100

    Add items to June

--- src/sections/Summer.js
+++ src/sections/Summer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Section, VerticalLine, CalendarDay, TimelineBlock } from '../ui';
+import { Section, VerticalLine, CalendarDay, TimelineBlock, FacebookPost } from '../ui';
 
 class Summer extends Component
 {
@@ -21,15 +21,62 @@ class Summer extends Component
                     <blockquote className="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">My commit is officially part of <a href="https://twitter.com/hashtag/reactnative?src=hash">#reactnative</a> <a href="https://twitter.com/hashtag/soproud?src=hash">#soproud</a><a href="https://t.co/kg8ijmVW91">https://t.co/kg8ijmVW91</a></p>&mdash; Jakob Kerkhove (@dejakob) <a href="https://twitter.com/dejakob/status/738276658797502464">June 2, 2016</a></blockquote>
                 </TimelineBlock>
 
+                <VerticalLine />
+
+                <TimelineBlock>
+                    <CalendarDay
+                        month="June"
+                        day={3}
+                    />
+                </TimelineBlock>
+                
+                <TimelineBlock>
+                    <FacebookPost
+                        href="https://www.facebook.com/dejakob/videos/1106582099380542/"
+                    />
+                </TimelineBlock>
+
+                <VerticalLine />
+
                 <TimelineBlock>
                     <CalendarDay
                         month="June"
                         day={10}
                     />
                 </TimelineBlock>
-                
+
                 <TimelineBlock>
                     <blockquote className="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Just completed a 42.21 km skate with Runkeeper. Check it out! <a href="https://t.co/59waiusFHb">https://t.co/59waiusFHb</a> <a href="https://twitter.com/hashtag/Runkeeper?src=hash">#Runkeeper</a></p>&mdash; Jakob Kerkhove (@dejakob) <a href="https://twitter.com/dejakob/status/741357178678841344">June 10, 2016</a></blockquote>
+                </TimelineBlock>
+
+                <VerticalLine />
+
+                <TimelineBlock>
+                    <CalendarDay
+                        month="June"
+                        day={12}
+                    />
+                </TimelineBlock>
+
+                <TimelineBlock>
+                    <FacebookPost
+                        href="https://www.facebook.com/photo.php?fbid=1111964478842304&set=a.145248142180614.16143.100000862866272&type=3"
+                    />
+                </TimelineBlock>
+
+                <VerticalLine />
+
+                <TimelineBlock>
+                    <CalendarDay
+                        month="June"
+                        day={18}
+                    />
+                </TimelineBlock>
+
+                <TimelineBlock>
+                    <FacebookPost
+                        href="https://www.facebook.com/dejakob/posts/1115283245177094"
+                    />
                 </TimelineBlock>
 
                 <VerticalLine />
